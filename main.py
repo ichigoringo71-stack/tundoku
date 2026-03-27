9e8d56c6f5ea6a92661e8792623e9ff5a2b41807
ichigoringo71-stack <ichigo.ringo71@gmail.com>
2026-03-28 01:35:34 +0900

    データベースの作成

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List, Optional
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import models, database
+
+#1. データベースのテーブルを作成
+models.Base.metadata.create_all(bind=database.engine)
 
 app=FastAPI()
 
-#1. データの形を定義
-class Book(BaseModel):
-    id: int
-    title: str
-    reason: str
-    status: str # 'unread', 'reading', 'completed'
-    review: Optional[str]=None
-
-#2. 仮のデータベースをメモリ上に保存
-books_db: List[Book]=[
-    Book(id=1, title="三体", reason="SFが好きだから", status="reading"),
-    Book(id=2, title="ファスト＆スロー", reason="心理学に興味がある", status="unread"),
-]
+#2. データベースセッションを取得する関数
+def get_db():
+    db=database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 #3. API(Application Programming Interface)エンドポイントの設定
-@app.get("/")
-def home():
-    return{"message": "tundoku API稼働中"}
-
-@app.get("/books", response_model=List[Book])
-def get_all_books():
-    """全ての本を取得する"""
-    return books_db
+@app.get("/books")
+def get_books(db: Session=Depends(get_db)):
+    """データベースからすべての本を取得"""
+    return db.query(models.Book).all()
 
 @app.post("/books")
-def create_books(book: Book):
+def create_books(title: str, reason: str, db: Session = Depends(get_db)):
     """新しい本を追加する"""
-    books_db.append(book)
-    return{"massage": f"[{book.title}]を追加しました", "book": book}
+    new_book=models.Book(title=title, reason=reason, status="unread")
+    db.add(new_book)
+    db.commit()
+    db.refresh(new_book)
+    return new_book
